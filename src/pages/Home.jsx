44ef0e49b7009f3e597d12f0e77e5f67ec6815d3
rahimbek027 <xabibullayevrahimbek5@gmail.com>
2024-09-27 08:44:28 +0500

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-8">Welcome to Twitter</h1>
+      <p>login : user </p>
+      <p>Parol : pass</p>
       
     </div>
   );
